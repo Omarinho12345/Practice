b8eab3b5ae43b4ef9cf2c9e9a0ce62f75921e2a6
Omarinho12345 <161320094+Omarinho12345@users.noreply.github.com>
2025-08-25 21:53:33 +0200

    Add files via upload

---
+++ grade.py
@@ -0,0 +1,9 @@
+grade=int(input("what grade you have?(only write the number)"))
+if grade > 59:
+    print("you passed.")
+else: 
+    print("you failed")
+
+
+
+ 
---
+++ phLevels.py
@@ -0,0 +1,7 @@
+question1=int(input("enter PH value from 0-14:"))
+if question1>7-14:
+  print("Basic")
+elif question1 <7:
+  print("Acidic") 
+else:
+  print("neutral")
