0be3e77240f7df665f6d680f0ed1d548b90f3f91
Omarinho12345 <161320094+Omarinho12345@users.noreply.github.com>
2025-08-30 13:11:07 +0200

    Add files via upload

---
+++ enter_pin.py
@@ -0,0 +1,9 @@
+print("Bank of omar")
+pin = int(input("Enter the pin: "))
+while pin !=3334:
+ pin = int(input("incorrect pin , try again: "))
+if pin == 3334:
+        print("correct pin , welcome back!")
+
+
+
