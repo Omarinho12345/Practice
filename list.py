6449049b9326ac0ba9abb53340d9234555bd250a
Omarinho12345 <161320094+Omarinho12345@users.noreply.github.com>
2025-09-03 13:26:53 +0200

    Add files via upload

---
+++ list.py
@@ -0,0 +1,24 @@
+fruits = ["apple ", "ananas" , "peach" ]
+
+print(fruits[0])
+
+print(fruits[1])
+
+print(fruits[2])
+
+print(fruits[0:3])
+ 
+print(fruits[::-1])
+
+for fruit in fruits:
+
+    print(fruits)
+
+    print(dir(fruits))
+    
+    #print(help(fruits)) se non ricordi
+
+print("peach" in fruits)
+
+print(len(fruits))
+
