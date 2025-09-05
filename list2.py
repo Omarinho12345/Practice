1d39ac5d13e6a0e10f03cbdaf1ae15ef44277bf1
Omarinho12345 <161320094+Omarinho12345@users.noreply.github.com>
2025-09-05 13:42:03 +0200

    Add files via upload

---
+++ list2.py
@@ -0,0 +1,16 @@
+fruits = ["apple" , "ananas" , "banana" , "watermelon"]
+fruits[1] = "banana"
+print(fruits[1])
+fruits.append("orange")
+print(fruits)
+fruits.remove("watermelon")
+print(fruits)
+
+fruits.reverse()
+
+print(fruits)
+
+fruits.clear
+print(fruits)
+print(fruits.index("apple"))
+print(fruits.count("apple"))
