56684910dfafaca4495d212970180e5c93a9016e
Omarinho12345 <161320094+Omarinho12345@users.noreply.github.com>
2025-09-06 08:18:09 +0200

    Add files via upload

---
+++ set.py
@@ -0,0 +1,13 @@
+fruits = {"apple", "ananas" , "banana"}
+print(fruits)
+#print(dir(fruits))
+#print(help(fruits))
+print(len(fruits))
+print("apple" in fruits)
+fruits.add("orange")
+print(fruits)
+fruits.remove("apple")
+print(fruits)
+fruits.pop()
+print(fruits)
+#fruits.clear
